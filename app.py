3e033d7144b61e117561d46dd8e1684d66ebc75d
Brian Birir <brianbirir@gmail.com>
2016-09-20 14:27:00 +0300

    Remove template

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 def index():
 	
     return render_template('index.html')
-	
-    return render_template('main-dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
